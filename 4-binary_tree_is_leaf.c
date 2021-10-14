cff078938798ca4e7d4f7987a1f564a2b82a4fa8
Lonzo Rust <rustlonzo@gmail.com>
2021-10-14 15:24:41 +0000

    Adding files for task 4.

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - returns 1 or zero
+ * @node: the node that is being checked to see if it's a leaf
+ * Return: 0 or 1
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left || node->right)
+		return (0);
+	else
+		return(1);
+}
