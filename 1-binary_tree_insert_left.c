687565d1af2b866cbad43fd038d8d1702f292c5d
Lonzo Rust <rustlonzo@gmail.com>
2021-10-14 14:39:59 +0000

    Adding files for task 1.

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts node to the left
+ * @parent: the parent node
+ * @value: value to be added to node
+ * Return: point to the node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
+	}
+	return(new_node);
+}
